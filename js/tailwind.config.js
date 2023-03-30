56c6b82144c1d29ec307b94175da815c858fa015
EyaminAman22 <maman201045@bscse.uiu.ac.bd>
2023-03-30 06:33:32 +0600

    shs

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'navy':'#040030',
+      },
+      fontFamily:{
+        'open':['Open Sans', "sans-serif"],
+        'pop':['Poppins', "sans-serif"],
+    },
+    fontSize:{
+      '18':'18px',
+      '20':'20px',
+      '24':'24px',
+    },
+    },
+  },
+}
