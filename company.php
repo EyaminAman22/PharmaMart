fc04354b71d7bde39e26da2b9c96ce1c756d8a60
Muzadded Chowdhury <alifchowdhury27@gmail.com>
2023-03-31 02:56:33 +0600

    html to php file

---
+++ company.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="../css/all.min.css">
+  <script src="js/tailwind.config.js"></script>
+  
+
+</head>
+<body class="m-0 p-0 font-pop">  
+  <div class="flex">
+    <div class="w-1/5 bg-navy text-white font-semibold text-center pt-4">
+        <h3 class="text-4xl pb-5">Pharma Mart</h3>
+        <i class="fa-solid fa-user text-white text-[100px]"></i>
+            <nav class="">
+                <ul class="flex pt-10 flex-col text-20">
+                    <a href="index.html"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400"> Dash Board</li></a>
+                    <a href="employee.html"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Employee</li></a>
+                    <a href="medicine.html"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Medicine</li></a>
+                    <a href="company.html"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Company</li></a>
+                    <a href="invoice.html"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Invoice</li></a>
+                    <a href="#"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Monthly Revenue</li></a>
+                    <a href="#"><li class="block  hover:bg-li-navy hover:ease-in py-3 ease-out duration-400">Orders</li></a>
+                </ul>
+            </nav>
+            <h2 class="pt-14 pb-1.5">Helpline</h2>
+            <h3 class="pt-2 pb-20">+880 20134-173959</h3>
+    </div>
+    <div class="w-4/5">
+        <div class="w-auto bg-navy flex justify-around p-5 text-white">
+            <input type="text" class="w-96 rounded-xl h-[30px] font-normal ml-[-100px]" placeholder="    Search Here">
+            <h1 class="font-semibold text-20 ml-[300px]">Hello, </h1>
+            <a href="#" class="font-semibold text-20 ml-[50px]">Log Out</a>
+        </div>
+        
+       
+           
+    </div>
+    
+</div>
+</body>
+</html>
