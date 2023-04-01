c1bd96ecd71e6dcb745564ab07164a7fff82027c
Muzadded Chowdhury <alifchowdhury27@gmail.com>
2023-04-02 03:10:33 +0600

    addmedicine done

--- addmedicine.php
+++ addmedicine.php
@@ -1,4 +1,76 @@
+<?php
+    session_start();
+    include("db_connect.php");
 
+    if(isset($_POST['submit'])){
+        
+        $medicine_name = $_POST['medicine_name'];
+        $dosage = $_POST['dosage'];
+        $mgf =$_POST['mgf'];
+        $exp = $_POST['exp'];
+        $category = $_POST['category'];
+        $quantity = $_POST['quantity'];
+        $base_price = $_POST['base_price'];
+        $sell_price = $_POST['sell_price'];
+        $company_id = $_POST['company_id'];
+        $provider_id = $_SESSION['provider_id'];
+
+        $error = [];
+
+        if(empty($medicine_name)){
+            $error['medicine_name'] = "Error";
+        }
+        if(empty($dosage)){
+            $error['dosage'] = "Error";
+        }
+        if(empty($mgf)){
+            $error['mgf'] = "Error";
+        }
+        if(empty($exp)){
+            $error['exp'] = "Error";
+        }
+        if(empty($category)){
+            $error['category'] = "Error";
+        }
+        if(empty($quantity)){
+            $error['quantity'] = "Error";
+        }
+        if(empty($base_price)){
+            $error['base_price'] = "Error";
+        }
+        if(empty($sell_price)){
+            $error['sell_price'] = "Error";
+        }
+        if(empty($company_id)){
+            $error['company_id'] = "Error";
+        }
+
+        if(count($error) == 0){
+            $add_info = mysqli_query($conn, "SELECT* FROM companies WHERE provider_id = '$provider_id' AND company_id = '$company_id'");
+
+            if(mysqli_num_rows($add_info)>0){
+                $insert = "INSERT INTO medicine(medicine_name,dosage,mgf,exp,category,quantity,base_price,sell_price,company_id,provider_id)
+                            VALUES('$medicine_name','$dosage','$mgf','$exp','$category','$quantity','$base_price','$sell_price','$company_id','$provider_id')";
+
+                            if(mysqli_query($conn,$insert)){
+                                echo "ADD DONE";
+                                $MSG = "Successfully Added Medicine";
+                            }
+            }
+            else{
+                echo "company not exist in your database";
+                $err_msg = "company not exist in your database" ;
+            }
+        }
+        else{
+            echo "Fill the Full form";
+        }
+    }
+    if(isset($_POST['reset'])){
+        header('addmedicine.php');
+    }
+
+?>
 <!doctype html>
 <html>
 <head>
@@ -37,7 +109,7 @@
         <div class="w-auto bg-navy flex justify-around p-5 text-white">
             <input type="text" class="w-96 rounded-xl h-[30px] font-normal ml-[-100px]" placeholder="    Search Here">
             <h1 class="font-semibold text-20 ml-[300px]">Hello, </h1>
-            <a href="#" class="font-semibold text-20 ml-[50px]">Log Out</a>
+            <a href="logout.php" class="font-semibold text-20 ml-[50px]">Log Out</a>
         </div>
         <div class="bg-navy mx-48 my-16 py-8 rounded-3xl text-white">
             <div class="flex justify-around">
@@ -45,7 +117,7 @@
                      <a href="index.php"><img  class="w-14 ml-64 mt-[-30px] ease-out duration-400 hover:w-16 hover:ease-in"  src="images/back.png" alt=""></a>
             </div>
             <div class="text-20">
-               <form class="flex flex-col text-center gap-y-4" action="" method="POST">
+               <form class="flex flex-col text-center gap-y-4" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                      <input class="w-96 ml-72 rounded-2xl" name="medicine_name" placeholder="Medicine Name"type="text">
             <input class="w-96 ml-72 rounded-2xl" name="dosage" placeholder="Dosage(mg/ml)"type="number">
             <input class="w-96 ml-72 rounded-2xl" name="mgf" placeholder="MGF"type="date">
@@ -60,8 +132,8 @@
               <input class="w-96 ml-72 rounded-2xl" name="sell_price" placeholder="Sell Price"type="number">
               <input class="w-96 ml-72 rounded-2xl" name="company_id" placeholder="Company ID"type="number">
                 <div class="flex justify-center gap-x-5 ">
-                    <input class="inline-block ml-[-40px] bg-sky-500 ease-out duration-400 hover:bg-blue-500 hover:ease-in p-2 rounded-xl" type="submit">
-                    <input class="inline-block bg-sky-500 ease-out duration-400 hover:bg-blue-500 hover:ease-in p-2 rounded-xl" type="reset" >
+                    <input class="inline-block ml-[-40px] bg-sky-500 ease-out duration-400 hover:bg-blue-500 hover:ease-in p-2 rounded-xl" type="submit" name = "submit">
+                    <input class="inline-block bg-sky-500 ease-out duration-400 hover:bg-blue-500 hover:ease-in p-2 rounded-xl" type="reset" name = "reset">
                 </div>
                </form>
             </div>
